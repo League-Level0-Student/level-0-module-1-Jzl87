295437a1fb57cbad38941b3306b00178622dee41
smili <smili@LAPTOP-AVKP4CIO.attlocal.net>
2023-06-10 11:32:15 -0700

    Pizza/TheRiddler/Tomato/robotinspace(Started)

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,40 @@
-
+  
+  PImage pepperoni;
+  PImage olive ;
+  PImage mushroom ;
+  
 void setup() {
-    
+  size(700,700);
+   background(255,255,255);
+   
+   noStroke();
+   fill(239,204,162);
+   ellipse(350,350, 500,500);
+   fill(178,24,7);
+   ellipse(350,350, 450,450);
+   fill(251, 219,101);
+   ellipse(350,350,420,420);
+   
+   pepperoni  = loadImage("pepperoni.png");
+   olive = loadImage("olive.png");
+    olive.resize(40,40);
+   mushroom = loadImage("mushroom.png");
 }
 void draw() {
+  
+  if(mousePressed && mouseButton == LEFT) {
+  image(pepperoni, 375,250);
+  image(mushroom, 400,100);
+  image(olive, 175,260);
+  image(pepperoni, 200,300);
+  
+  }
+  if (mousePressed && mouseButton ==RIGHT) {
+    image(mushroom, 150,750);
+  image(olive, 405,290);
+  image(pepperoni, 305,350);
+  image(mushroom, 300,400);
+  image(olive, 275, 160);
     
+  }
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -6,20 +9,31 @@ package _04_int._1_riddler;
 
 public class TheRiddler {
 
+	
 	public static void main(String[] args) {
-
-		// 1. Make a variable to hold the score
-
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
 		
+		// 1. Make a variable to hold the score
+		int score = 0; 
+		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String riddle = JOptionPane.showInputDialog("Who has a neck, yet no head?");
+			if (riddle.equals("A bottle")) {
+				JOptionPane.showMessageDialog(null, "Congratulations!");
+				score += 1;
+			} else {
+			JOptionPane.showMessageDialog(null, "WRONG!");
+			}
+			
+			String riddle2 = JOptionPane.showInputDialog("What has hand butt cannot clap?");
+			if (riddle2.equals("A clock")) {
+				JOptionPane.showMessageDialog(null, "Congratulations!");
+				score += 1;
+			} else {
+			JOptionPane.showMessageDialog(null, "WRONG!");
+			}
+		// 6. Add some more riddles
+			
+		// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null, "This is your score: " + score);
 	}
 }
 
